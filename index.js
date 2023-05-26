a91df5509f42ca2f43bca2e2a7e6391f6e4f0c5e
RodneyEcheru <ivilleinc@gmail.com>
2023-05-27 00:47:02 +0300

    reactivity attempt

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ const statefulVariable = (key, initialValue) => {
         set,
         update,
         subscribe,
+        reactive: {
+            subscribe: (callback) => {
+                return this.subscribe((value) => {
+                    callback(value);
+                });
+            }
+        }
     };
 };
 
